8da38119ee0cc39f1f9f030c3bdd88bfccc2bf06
DebbieCoates <debbie.coates@hotmail.co.uk>
2025-09-16 11:58:30 +0100

    created media folder

--- website/vas/models.py
+++ website/vas/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-
-
-from django.db import models
-
 class Contact(models.Model): 
     first_name = models.CharField(max_length=100, null=True, blank=True)
     last_name = models.CharField(max_length=100, null=True, blank=True)
@@ -15,6 +11,8 @@ class Contact(models.Model):
     county = models.CharField(max_length=100, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
+    
+    image = models.ImageField(upload_to='contact_images/', null=True, blank=True)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name} "
